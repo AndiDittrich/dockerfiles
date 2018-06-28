367c0646891b3f247f1aef66512958e39b4d7d87
Andi Dittrich <andi@andidittrich.de>
2018-06-28 22:25:56 +0200

    removed rootfs from git - temporary build only

--- alpine-raw/build.sh
+++ alpine-raw/build.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env bash
 
-# to merge the default alpine rootfs with some modified files
+# merge the default alpine rootfs with some modified files
 # in the future this can be replaced by a multistage build but during some bugs https://github.com/moby/moby/issues/34645
 # with user-namespace-remapping this has to be done outside the docker environment
 
 # fail on errors
 set -xe
 
+# vars
+ALPINE_RELEASE_VERSION="3.8"
+ALPINE_PATCH_VERSION="3.8.0"
+ALPINE_FILENAME="alpine-minirootfs-${ALPINE_PATCH_VERSION}-x86_64.tar.gz"
+ALPINE_URL="http://dl-cdn.alpinelinux.org/alpine/v${ALPINE_RELEASE_VERSION}/releases/x86_64/${ALPINE_FILENAME}"
+
 # create temp dir
-mkdir -p .tmp
+mkdir -p build/fs
+
+# fetch latest alpine image
+wget $ALPINE_URL -O build/${ALPINE_FILENAME}
+
+# checksum verification
+sha256sum -c checksums.sha256
 
 # uncompress alpine rootfs
 # copy additional files
 # create new rootfs
-fakeroot sh -c '
-    tar xfz alpine-minirootfs-3.8.0-x86_64.tar.gz -C .tmp
-    cp -R fs/* .tmp
-    cd .tmp
+fakeroot sh -c "
+    tar xfz build/${ALPINE_FILENAME} -C build/fs
+    cp -R fs/* build/fs
+    cd build/fs
     tar cf ../rootfs.tar * --owner=root --group=root
-'
-
-# delete temp dir
-rm -rf .tmp
+"
 
 # trigger docker build
-docker build -t alpine-base:3.8 .
+docker build -t alpine-raw:${ALPINE_RELEASE_VERSION} .
+
+# delete temp dir
+rm -rf build
